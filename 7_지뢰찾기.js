3aa76ad9e5ac9698532be5e7934419f07b466061
EunjiBaek <70505312+EunjiBaek@users.noreply.github.com>
2021-03-22 20:18:45 +0900

    Update 7_지뢰찾기.js

--- 7_지뢰찾기.js
+++ 7_지뢰찾기.js
@@ -2,6 +2,8 @@ var tbody = document.querySelector('#table tbody');
 var dataset = [];
 
 document.querySelector('#exec').addEventListener('click', () => {
+    tbody.innerHTML = '';
+    dataset = [];
     var hor = parseInt(document.querySelector('#hor').value);
     var ver = parseInt(document.querySelector('#ver').value);
     var mine = parseInt(document.querySelector('#mine').value);
@@ -38,6 +40,35 @@ document.querySelector('#exec').addEventListener('click', () => {
                 e.currentTarget.textContent = '!';
                 dataset[줄][칸] = '!';
             });
+            td.addEventListener('click', (e) => {
+                //클릭했을때 주변 지뢰 개수
+                var 부모tr = e.currentTarget.parentNode;
+                var 부모tbody = e.currentTarget.parentNode.parentNode;
+                var 칸 = Array.prototype.indexOf.call(부모tr.children, e.currentTarget);
+                var 줄 = Array.prototype.indexOf.call(부모tbody.children, 부모tr);
+                e.currentTarget.classList.add('opened');
+                if(dataset[줄][칸] === 'X'){
+                    e.currentTarget.textContent = '펑';
+                }else{
+                    var 주변 = [
+                        dataset[줄][칸-1], dataset[줄][칸+1]
+                    ];
+                    if(dataset[줄-1]) {
+                        주변 = 주변.concat(dataset[줄-1][칸-1],dataset[줄-1][칸],dataset[줄-1][칸+1]);
+                    }
+                    if(dataset[줄+1]){
+                        주변 = 주변.concat(dataset[줄+1][칸-1],dataset[줄+1][칸],dataset[줄+1][칸+1]);
+                    }
+                    let 주변지뢰개수 = e.currentTarget.textContent = 주변.filter( (v) => {
+                        return v === 'X';
+                    }).length;
+
+                    if(주변지뢰개수 === 0){
+                        // 주변 8칸 동시 오픈 (재귀함수)
+
+                    }
+                }
+            });
             tr.appendChild(td);
         }
         tbody.appendChild(tr);
@@ -51,10 +82,19 @@ document.querySelector('#exec').addEventListener('click', () => {
         tbody.children[세로].children[가로].textContent = 'X';
         dataset[세로][가로] = 'X';
     }
-    console.log(dataset);
-
 });
 
 
+// 무한 반복문 (반복문을 함수로 실행하는 방법이라고 할수 있다)
+// function 재귀함수 () {
+//     console.log(숫자);
+//     if (숫자 < 5) {
+//         재귀함수 (숫자 + 1);
+//     }
+// }
 
+
+
+// 반복문과 비동기함수가 만날때 클로저 문제가 자주 발생한다
 //마우스 오른쪽 클릭 이벤트는 contextmenu 이벤트이다.
+// concat은 배열과 배열을 합쳐서 새로운 배열을 만든다
